25b72432313143bf4af2187d2127096e63a2ed84
Sean <sean@hydreon.com>
2015-11-04 08:03:51 -0500

    Fix issues.
    - better syntax
    - remove soup cases (for now)
    - better variables
    - remove excess code
    - general cleanup
    
    **Note it is still broken!

--- src/Sean_M/QuickHeal/Main.php
+++ src/Sean_M/QuickHeal/Main.php
@@ -8,66 +8,39 @@ use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\event\player\PlayerInteractEvent;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener {
 
     public $config;
 
-    public function onLoad(){
+    public function onLoad() {
         $this->getLogger()->info(TextFormat::GREEN . "Loading QuickHeal by Sean_M...");
     }
 
-     public function onEnable(){
+     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
         $this->config = $this->getConfig()->getAll();
         $this->getLogger()->info(TextFormat::GREEN . "QuickHeal by Sean_M enabled!");
      }
 
-     public function onDisable(){
-        $this->getLogger()->info(TextFormat::GREEN . "QuickHeal by Sean_M disabled!");
+     public function onDisable() {
+        $this->getLogger()->info(TextFormat::RED . "QuickHeal by Sean_M disabled!");
      }
 
-     public function onInteract(PlayerInteractEvent $event){
-// Item naming and enchantments will be in QuickHeal v1.5.0 — coming once MCPE v0.12 is publicly released.
-        $config = $this->getConfig();
-        $player = $event->getPlayer();
-        $item = $config->get("id");
-        $playerhealth = $player->getHealth();
-        $health = $config->get("health");
-        $sethealth = $playerhealth + $health;
-          if($item == "282" and $playerhealth !== 20){
-            $id = 282;
-            $damage = 0;
-            $count = 1;
-            $mstew = Item::get($id, $damage, $count);
-            $player->setHealth($sethealth);
-            $player->getInventory()->removeItem($mstew);
-            $id = 281;
-            $damage = 0;
-            $count = 1;
-            $bowl = Item::get($id, $damage, $count);
-            $player->getInventory()->addItem($bowl);
-               if($item == "459" and $playerhealth !== 20){
-                 $id = 459;
-                 $damage = 0;
-                 $count = 1;
-                 $bstew = Item::get($id, $damage, $count);
-                 $player->setHealth($sethealth);
-                 $player->getInventory()->removeItem($bstew);
-                 $id = 281;
-                 $damage = 0;
-                 $count = 1;
-                 $bowl = Item::get($id, $damage, $count);
-                 $player->getInventory()->addItem($bowl);
-          }else{
-        if($item !== "282" or "459"){
-          $damage = 0;
-          $count = 1;
-          $itemusing = Item::get($item, $damage, $count);
-          $player->setHealth($sethealth);  
-          $player->getInventory()->removeItem($itemusing);
-// Implement custom addItem code in the future.
-            }
+     public function onInteract(PlayerInteractEvent $event) {
+// Proper item naming and enchantments will be in QuickHeal v1.5.0 — coming once PocketMine implements naming & enchantments.
+       $config = $this->getConfig();
+       $player = $event->getPlayer();
+       $item = $config->get("id");
+       $playerHp = $player->getHealth();
+       $health = $config->get("health");
+       $setHp = $playerHp + $health;
+         if($playerHp <= 20) {
+           $damage = 0;
+           $count = 1;
+           $removeItem = Item::get($item, $damage, $count);
+             $player->setHealth($setHp);
+             $player->getInventory()->removeItem($removeItem);
          } 
       }
    }
